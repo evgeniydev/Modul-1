3d30aa600e1742855f44902a5f1e80b2a170cf6b
Евгений Толкач <evgeniy.front.developer@gmail.com>
2026-08-06 21:43:12 +0300

    сделали 1.2

---
+++ index.js
@@ -0,0 +1,18 @@
+{
+  const item = 'smart проектор philips';
+  const count = 15;
+  // const category = 'видео техника';
+  const price = 1200;
+
+  console.log(item);
+  console.log(`общая сумма товара: ${count * price}`);
+}
+
+{
+  const item = 'смартфон Xiaomi Redmi 14';
+  const count = 25;
+  const price = 1000;
+
+  console.log(item);
+  console.log(`общая сумма товара: ${count * price}`);
+}
