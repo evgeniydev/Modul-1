6d756374a8d49f5678f78ed11e02a26af79d6177
Евгений Толкач <evgeniy.front.developer@gmail.com>
2026-08-25 11:54:17 +0300

    сделали 1.4

--- lesson_1_4/index.js
+++ lesson_1_4/index.js
@@ -1,10 +1,14 @@
-'use strict';
+"use strict";
 
 {
   const nameItem = prompt('Введите наименование товара');
   const itemCount = +prompt("Введите количество товара");
   const itemCategory = prompt("Введите категорию товара");
   const itemPrice = +prompt("Введите цену за единицу товара");
-  console.log(typeof(itemCount));
-  console.log(`На складе ${itemCount} единиц(ы) товара ${nameItem} на сумму ${itemPrice * itemCount}`); 
+
+  if (!isNaN(itemCount) && !isNaN(itemPrice)) {
+    console.log(`На складе ${itemCount} единиц(ы) товара ${nameItem} на сумму ${itemPrice * itemCount}`);
+  } else {
+    console.log("Вы ввели некорректные данные");
+  }
 }
